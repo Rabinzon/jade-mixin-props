92dc9f799983f494ab587de8b5efbd3671cd0f1e
rabinzon <ildarrazin@gmail.com>
2016-12-17 19:45:19 +0300

    feat: remove deep

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ module.exports = function props(props) {
 				return acc;
 			}, {})
 		};
-		
-		this.deep = function (keys, defualt) {
-			return keys.reduce(function(acc, cur) {
-				acc = acc[cur] ? acc[cur]
-					: (typeof defualt !== 'undefined' ? defualt : false);
-				return acc;
-			}, data);
-		}
 	}
 	
 	return function(key, defualt) {
 		if (!key) {
 			return new Inner();
+		}
+		
+		if (key instanceof Array) {
+			return key.reduce(function(acc, cur) {
+				acc = acc[cur] ? acc[cur]
+					: (typeof defualt !== 'undefined' ? defualt : false);
+				return acc;
+			}, data);
 		}
 		return data[key] ? data[key]
 			: (typeof defualt !== 'undefined' ? defualt : false);
