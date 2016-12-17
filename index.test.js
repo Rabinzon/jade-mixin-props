59c31c455c0bc67afed2b799b4ac2dacb1f03d2f
rabinzon <ildarrazin@gmail.com>
2016-12-17 17:47:40 +0300

    test: add tests

---
+++ index.test.js
@@ -0,0 +1,36 @@
+var props = require('./index');
+
+test('should get value by key', function () {
+	var data = {x: 1, y: 2};
+	var prop = props(data);
+	expect(prop('x')).toBe(1);
+});
+
+test('should get empty value', function () {
+	var prop = props({});
+	expect(prop('a')).toBe(false);
+});
+
+test('should get default value', function () {
+	var prop = props({});
+	expect(prop('q', 'test value')).toBe('test value');
+});
+
+test('should get deep value', function () {
+	var data = {x: {b: {c: 1}}, y: 2};
+	var prop = props(data);
+	expect(prop().deep(['x', 'b', 'c'])).toBe(1);
+});
+
+test('should get default value, deep, all', function () {
+	var data = {x: {b: {c: 1}}, y: 2};
+	var prop = props(data);
+	expect(prop().deep(['x', 'b', 'c', 'e'], 'empty')).toBe('empty');
+});
+
+test('should get object by keys', function () {
+	var data = {x: 1, y: 2, z: 'string'};
+	var prop = props(data);
+	var result = prop().all(['x', 'y', 'z']);
+	expect(result).toEqual(data);
+});
